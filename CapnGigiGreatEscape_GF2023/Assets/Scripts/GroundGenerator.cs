0721c3a8ac22a9afffcf8b05f18f41368a016943
MooFoo612 <83637762+MooFoo612@users.noreply.github.com>
2023-02-03 23:40:10 +0000

    Revert "Merge branch 'Diarmaid' into Weekend_1"
    This reverts commit f0acc0d0d7df5ff29280dc839e316167af15e460, reversing
    changes made to 856f6f4d5b0470966344af9dc9fb292670d137c4.

--- CapnGigiGreatEscape_GF2023/Assets/Scripts/GroundGenerator.cs
+++ CapnGigiGreatEscape_GF2023/Assets/Scripts/GroundGenerator.cs
@@ -11,15 +11,15 @@ public class GroundGenerator : MonoBehaviour
     Vector2 velocity;
 
     // General
-    private float groundHeight;
+    public float groundHeight;
     
     // RHS & LHS of ground collider
-    private float groundRight;
-    //public float groundLeft;
+    public float groundRight;
+    public float groundLeft;
 
     //RHS & LHS of visible play area
-    private float screenRight;
-    //public float screenLeft;
+    public float screenRight;
+    public float screenLeft;
 
     // Colliders
     BoxCollider2D groundCollider;
@@ -39,7 +39,7 @@ public class GroundGenerator : MonoBehaviour
         // Assign calculations to world variables
         groundHeight = transform.position.y + (groundCollider.size.y / 2);
         screenRight = Camera.main.transform.position.x * 2;
-        //screenLeft = Camera.main.transform.position.x * -2;
+        screenLeft = Camera.main.transform.position.x * -2;
     }
 
     // Start is called before the first frame update
@@ -57,35 +57,26 @@ public class GroundGenerator : MonoBehaviour
     private void FixedUpdate()
     {
         // Get the current direction from the ground
-        //Vector2 currentPosition = transform.position;
-        //currentPosition.x -= player.rb.velocity.x * Time.fixedDeltaTime;
+        Vector2 currentPosition = transform.position;
+        currentPosition.x -= player.rb.velocity.x * Time.fixedDeltaTime;
+
 
         // Fetch the position of the furthest point to the right of the ground section every frame
         groundRight = transform.position.x + (groundCollider.size.x / 2);
         
-        // If ground section moves offscreen destroy it
-        if (groundRight < 0)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
         // If a ground section has not been generated
         if (!groundGenerated)
         {
             // and the end of the screen has moved beyond the furthest point right of the ground level
             if (groundRight < screenRight) 
             {
-
-                // Generate the next section of the ground level
-                generateSection();
                 // Set bool to true to prevent extra sections being spawned
                 groundGenerated = true;
+                // Generate the next section of the ground level
+                generateSection();
             }
         }
-        //transform.position = currentPosition;
     }
-
 
     void generateSection()
     {
