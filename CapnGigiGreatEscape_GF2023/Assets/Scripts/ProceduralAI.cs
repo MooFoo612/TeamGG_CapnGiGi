0339e1a2a56f586653e8be2e96687bf94f39554b
MooFoo612 <83637762+MooFoo612@users.noreply.github.com>
2023-02-03 18:54:30 +0000

    test

--- CapnGigiGreatEscape_GF2023/Assets/Scripts/ProceduralAI.cs
+++ CapnGigiGreatEscape_GF2023/Assets/Scripts/ProceduralAI.cs
@@ -7,7 +7,7 @@ public class ProceduralAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
