e0c860b3d40cbb30f515420d6d832203d38294ab
MooFoo612 <83637762+MooFoo612@users.noreply.github.com>
2023-02-06 20:53:06 +0000

    Level Generation
    - Split level generator script into platform and ground scripts so both can be generated independently
    - Created test objects and edited test environment

--- CapnGigiGreatEscape_GF2023/Assets/Scripts/BlocksAI/PlatformGenerator.cs
+++ CapnGigiGreatEscape_GF2023/Assets/Scripts/BlocksAI/PlatformGenerator.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    [SerializeField] private Transform platformSection;
+    [SerializeField] private Transform platformStart;
+    [SerializeField] private GameObject player;
+    private const float DISTANCE_TO_SPAWN_SECTION = 25f;
+    private Vector3 lastEndPosition;
+    #endregion
+    void Awake()
     {
-        
+        // Access the player
+        player = GameObject.Find("CapnGigi");
+        // Find the child EndPosition object in the GameStart parent
+        lastEndPosition = platformStart.Find("EndPosition").position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // If the player is close enough to the next reference spawn point
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < DISTANCE_TO_SPAWN_SECTION)
+        {
+            // Spawn another section
+            SpawnSection();
+        }
     }
+    #region Spawner
+    private void SpawnSection()
+    {
+        //Get the transform to refrence the next End Position
+        Transform lastSectionTransform = SpawnSection(lastEndPosition);
+        lastEndPosition = lastSectionTransform.Find("EndPosition").position;
+    }
+    private Transform SpawnSection(Vector3 newSection)
+    {
+        Transform lastSectionTransform = Instantiate(platformSection, newSection, Quaternion.identity);
+        return lastSectionTransform;
+    }
+    #endregion
 }
+
