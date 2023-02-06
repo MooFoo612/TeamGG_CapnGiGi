caa72e32f8c081b0e287260a08d8681bb487b60f
MooFoo612 <83637762+MooFoo612@users.noreply.github.com>
2023-02-06 13:08:38 +0000

    Manual generation done
    1. Open Prefabs->Diarmaid_Test
    2. Drag LevelGenerator into Scene
    3. Drag EndPosition prefab into scene
    4. Select the LevelGenerator in the hierarchy and drag Level Section into the Inspector

--- CapnGigiGreatEscape_GF2023/Assets/Scripts/BlocksAI/GroundGenerator.cs
+++ CapnGigiGreatEscape_GF2023/Assets/Scripts/BlocksAI/GroundGenerator.cs
@@ -3,30 +3,28 @@ using UnityEngine;
 public class GroundGenerator : MonoBehaviour
 {
     #region Variables
-    [SerializeField] private Transform LvlBlock_1;
+    [SerializeField] private Transform lvlSection1;
+    [SerializeField] private Transform lvlSection2;
+
     #endregion
 
     void Awake() 
     {
-        Instantiate(LvlBlock_1, new Vector3(-1, -5), Quaternion.identity);
+        //Vector3 newSection = lvlSection.Find("EndPosition").position;
+        SpawnSection1(new Vector3(18,3));
+        SpawnSection2(new Vector3(18,3) + new Vector3(24,3));
+        SpawnSection1(new Vector3(18,3) + new Vector3(48,3));
     }
 
-    // Start is called before the first frame update
-    void Start()
+    #region Spawner
+    private void SpawnSection1(Vector3 newSection)
     {
-        
+        Instantiate(lvlSection1, newSection, Quaternion.identity);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void SpawnSection2(Vector3 newSection)
     {
-        
+        Instantiate(lvlSection2, newSection, Quaternion.identity);
     }
-
-    private void FixedUpdate()
-    {
-       
-    }
-
-   
+    #endregion
 }
