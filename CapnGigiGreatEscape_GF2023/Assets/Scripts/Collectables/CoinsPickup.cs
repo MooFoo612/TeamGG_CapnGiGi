c7567cb2a40b341439b88951670c3690840c99d5
EndaCr <123556231+EndaCr@users.noreply.github.com>
2023-02-26 22:27:52 +0000

    Coin animation

--- CapnGigiGreatEscape_GF2023/Assets/Scripts/Collectables/CoinsPickup.cs
+++ CapnGigiGreatEscape_GF2023/Assets/Scripts/Collectables/CoinsPickup.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class CoinsPickup : MonoBehaviour
 {
     public int coinsAmount = 20;
+    private Animator animatorCoin;
+
+
+        void Start()
+    {
+
+                animatorCoin = gameObject.GetComponent<Animator>();
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision){
         // Get the script from the collision object 
@@ -12,8 +21,9 @@ public class CoinsPickup : MonoBehaviour
         if(player){
             // Add health to the character 
             player.Coins += coinsAmount;
+             animatorCoin.SetTrigger("Collect");
             // Destroy the collectable
-            Destroy(gameObject);
+            Destroy(gameObject, 0.5f);
         }
     }
 }
